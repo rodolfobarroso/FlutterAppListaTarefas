b47ab693bb4cfe88b66af172e0a5a2579b6e556c
RodolfoBarroso <rodolfocesar24@gmail.com>
2019-10-21 17:04:04 -0300

    Projeto Iniciado

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:io' as prefix0;
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _toDoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("Lista de Tarefas"),
+        centerTitle: true,
+      ),
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("{$directory.path}/data.jason");
+  }
+
+  Future<File> _saveData() async {
+    String data = jsonEncode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+}
